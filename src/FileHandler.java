7cd603a645b898654fdd4ebf95e746dbb14d1fab
GeorgeElsham <40073010+GeorgeElsham@users.noreply.github.com>
2021-10-25 00:21:12 +0100

    Added file handler to read example programs

---
+++ src/FileHandler.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+
+public class FileHandler {
+  public static String readFile(String name) {
+    try {
+      final File file = new File(name);
+      final Scanner scanner = new Scanner(file);
+
+      List<String> lines = new ArrayList<>();
+      while (scanner.hasNextLine()) {
+        final String line = scanner.nextLine();
+        lines.add(line);
+      }
+
+      scanner.close();
+      return String.join("\n", lines);
+    } catch (FileNotFoundException e) {
+      e.printStackTrace();
+      return null;
+    }
+  }
+}
