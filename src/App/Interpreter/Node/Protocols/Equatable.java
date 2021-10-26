861b51ec710387e05267048ead4b08603d24b979
GeorgeElsham <40073010+GeorgeElsham@users.noreply.github.com>
2021-10-26 16:50:00 +0100

    Added comments for protocols

--- src/App/Interpreter/Node/Protocols/Equatable.java
+++ src/App/Interpreter/Node/Protocols/Equatable.java
@@ -1,5 +1,11 @@
 package App.Interpreter.Node.Protocols;
 
+/**
+ * Interface to mark a type as equatable. This
+ * means they can be compared for equality.
+ *
+ * @param <Value> Value to compare.
+ */
 public interface Equatable<Value> {
   /**
    * Gets value given by implementation to
@@ -9,10 +15,26 @@ public interface Equatable<Value> {
    */
   Value getEquatableValue();
 
+  /**
+   * Get if two objects are equal.
+   *
+   * @param left Left object.
+   * @param right Right object.
+   * @param <Value> Object types.
+   * @return Whether they are equal.
+   */
   static <Value extends Equatable<?>> boolean isEqual(Value left, Value right) {
     return left.getEquatableValue().equals(right.getEquatableValue());
   }
 
+  /**
+   * Get if two objects are not equal.
+   *
+   * @param left Left object.
+   * @param right Right object.
+   * @param <Value> Object types.
+   * @return Whether they are not equal.
+   */
   static <Value extends Equatable<?>> boolean isNotEqual(Value left, Value right) {
     return !isEqual(left, right);
   }
--- src/App/Interpreter/Node/Protocols/IntegerValue.java
+++ src/App/Interpreter/Node/Protocols/IntegerValue.java
@@ -1,5 +1,8 @@
 package App.Interpreter.Node.Protocols;
 
+/**
+ * Interface to mark a type as having an associated integer value.
+ */
 public interface IntegerValue {
   /**
    * Get integer value.
